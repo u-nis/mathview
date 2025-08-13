99d12c6f28dd50d5127798d8aa51649a91fc3745
u.nis <you.nis@outlook.com>
2025-08-13 01:22:18 -0600

    UI improvements and side rulers for margins

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,9 +1,11 @@
 import MathEditor from '@/components/mathview/MathEditor'
 import Editor from '../components/Editor/Editor'
+import TopBar from '../components/TopBar/TopBar'
 
 export default function Home() {
   return (
     <div>
+    <TopBar />
     <Editor />
     </div>
   )
